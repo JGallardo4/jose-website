818842d8a64680baee66bb019883c84f978bdcf9
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-06-05 18:01:27 -0600

    Update

---
+++ src/app/components/portfolio-item.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const PortfolioItem = () => {
+  return (
+    <article>
+
+    </article>
+  )
+}
+
+export default PortfolioItem
---
+++ src/app/components/works/works.tsx
@@ -0,0 +1,11 @@
+import PortfolioItem from '../components/portfolio-item.tsx'
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <section>
+                <header>Works</header>
+            </section>
+        </main>
+    )
+}
