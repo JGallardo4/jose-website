75e0377dffdd6b6bc6cb65c5111e4118a2fcfcff
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-06-14 13:13:42 -0600

    Update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    pageExtensions: ['tsx'],
     experimental: {
         appDir: true,
-        mdxRs: true,
     },
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import PortfolioItemsList from '@/components/portfolio-items-list'
 import { Suspense } from 'react'
 import SocialMediaList from './components/social-media-list'
+import { revalidateTag } from 'next/cache'
 
 export default function Home() {
   return (
