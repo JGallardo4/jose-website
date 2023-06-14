4710811648e81c3c0f81f531086718c06208daa3
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-06-14 10:26:06 -0600

    Test data revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,6 +4,7 @@ import { revalidateTag } from 'next/cache'
 export async function GET(request: NextRequest) {
     const tag = request.nextUrl.searchParams.get('tag')
     revalidateTag(tag ?? "")
-    console.log("Revalidating!!!")
+    console.log(`Revalidating tag: ${tag}`)
+    console.log(Date.now())
     return NextResponse.json({ revalidated: true, now: Date.now() })
 }
