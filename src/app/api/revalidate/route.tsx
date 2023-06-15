2c3cc7f679b9701945705acf02632aa3850fc247
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-06-15 09:29:29 -0600

    Test env variables

--- src/app/api/revalidate/route.tsx
+++ src/app/api/revalidate/route.tsx
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import { revalidateTag } from 'next/cache'
 
 export async function GET(request: NextRequest) {
-    const tag = request.nextUrl.searchParams.get('tag')
-    await revalidateTag(tag ?? "revalidate")
-    return NextResponse.json({ revalidated: true, now: Date.now() })
+    console.log(process.env.CMS_API_URL)
+    // const tag = request.nextUrl.searchParams.get('tag')
+    // revalidateTag(tag ?? "revalidate")
+    // return NextResponse.json({ revalidated: true, now: Date.now() })
 }
