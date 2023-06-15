8a625c551236b6782b2cf2c773a8d4d3b82a6ebb
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-06-15 10:04:47 -0600

    Test env variables

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,10 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { revalidateTag } from 'next/cache'
+import { NextRequest, NextResponse } from 'next/server';
+import { revalidateTag } from 'next/cache';
 
 export async function GET(request: NextRequest) {
-    console.log("Env variable:")
-    console.log(process.env.CMS_API_URL)
-    // const tag = request.nextUrl.searchParams.get('tag')
-    // revalidateTag(tag ?? "revalidate")
-    return NextResponse.json({ revalidated: true, now: Date.now() })
+  // const tag = request.nextUrl.searchParams.get('tag')
+  // revalidateTag(tag ?? "revalidate")
+  return NextResponse.json({
+    revalidated: true,
+    now: Date.now(),
+    CMS_API_URL: process.env.CMS_API_URL,
+  });
 }
