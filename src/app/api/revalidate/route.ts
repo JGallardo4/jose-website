6fdce391e454696a9794325126a4953ca4f75b7b
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-06-15 18:37:32 -0600

    Update

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,18 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { revalidateTag } from 'next/cache';
+import { NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
 
-export async function GET(request: NextRequest) {
-  const tag = request.nextUrl.searchParams.get('tag')
-  revalidateTag(tag ?? "revalidate")
-
-
-  const baseUrl = process.env.CMS_API_URL
-  const response = await fetch(baseUrl + 'social-media-links?populate=*', { next: { tags: ['revalidate'] } });
-  const entries = await response.json()
+export async function POST() {
+  revalidatePath('/')
 
   return NextResponse.json({
     revalidated: true,
     now: Date.now(),
-    links: entries,
   });
 }
