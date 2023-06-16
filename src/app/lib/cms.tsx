61a045a82f3df1b929382327640b48f97fdf152a
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-06-16 09:52:44 -0600

    Test

--- src/app/lib/cms.tsx
+++ src/app/lib/cms.tsx
@@ -1,46 +1,49 @@
+import type { PortfolioItem, SocialMediaLink } from '@/lib/types.d';
 
-import type { PortfolioItem, SocialMediaLink } from '@/lib/types.d'
-
-export const fetchCache = 'force-no-store'
-
-const baseUrl = process.env.CMS_API_URL
+const baseUrl = process.env.CMS_API_URL;
 
 export const getAllPortfolioEntries = async () => {
-    try {
-        const response = await fetch(baseUrl + 'portfolio-entries?populate=*', { next: { tags: ['revalidate'] } });
+  try {
+    const response = await fetch(baseUrl + 'portfolio-entries?populate=*', {
+      next: { tags: ['revalidate'] },
+      cache: 'no-store',
+    });
 
-        if (!response.ok) {
-            throw new Error(response.statusText)
-        }
-
-        const entries = await response.json()
-
-        if (entries === null) {
-            throw new Error('No entries found')
-        }
-
-        return entries.data as PortfolioItem[]
-    } catch (error) {
-        console.error(error)
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
-}
+
+    const entries = await response.json();
+
+    if (entries === null) {
+      throw new Error('No entries found');
+    }
+
+    return entries.data as PortfolioItem[];
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const getAllsocialMediaLinks = async () => {
-    try {
-        const response = await fetch(baseUrl + 'social-media-links?populate=*', { next: { tags: ['revalidate'] } });
+  try {
+    const response = await fetch(baseUrl + 'social-media-links?populate=*', {
+      next: { tags: ['revalidate'] },
+      cache: 'no-store',
+    });
 
-        if (!response.ok) {
-            throw new Error(response.statusText)
-        }
-
-        const entries = await response.json()
-
-        if (entries === null) {
-            throw new Error('No entries found')
-        }
-
-        return entries.data as SocialMediaLink[]
-    } catch (error) {
-        console.error(error)
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
-}
+
+    const entries = await response.json();
+
+    if (entries === null) {
+      throw new Error('No entries found');
+    }
+
+    return entries.data as SocialMediaLink[];
+  } catch (error) {
+    console.error(error);
+  }
+};
